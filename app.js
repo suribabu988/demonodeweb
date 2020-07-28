d42eb4e00ea91a5c08799f778949f75fbc094896
suribabu <suri.babu988@gmail.com>
2020-07-28 11:21:24 +0530

    text changed

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) =>{
-res.send('this is home page');
+res.send('this is home');
 });
 
 app.get('/posts', (req, res) => {
-    res.send('this is posts page');
+    res.send('this is posts');
 });
 
 
